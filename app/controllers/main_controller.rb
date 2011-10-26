23b00575edce7dbccfeabc89e9deef34e2e4ec23
aalbertobus <aalbertobus@gmail.com>
2011-10-26 16:35:17 -0600

    Añadido crear,editar y borrar movimiento

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,8 +5,44 @@ class MainController < ApplicationController
   def list
     @movements = Movement.order("movements.amount ASC")
   end
+  
+  def new
+    @movement = Movement.new 
+  end
 
- 
+  def create
+  @movement = Movement.new(params[:movement])
+  @movement.save
+  if @movement.save
+      redirect_to(:action => 'list')
+  else
+    render('new')
+    end
+  end
+  
+  
+  def edit
+      @movement = Movement.find(params[:id])
+  end
+
+  def update
+      
+      @movement = Movement.find(params[:id])
+      if @movement.update_attributes(params[:movement])
+      redirect_to(:action => 'list', :id => @movement.id)
+      else
+         render('edit')
+      end
+ end
+  
+  def delete
+    @movement = Movement.find(params[:id])
+  end
+  
+  def destroy
+    @movement = Movement.find(params[:id]).destroy
+    redirect_to(:action => 'list')
+  end
  
   
   
