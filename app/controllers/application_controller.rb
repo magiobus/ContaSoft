2ed3c653ceaafad570b6f6f9a6955fe85731f806
Alberto Bustillos <aalbertobus@gmail.com>
2011-11-05 10:57:49 -0700

    Reacomodo de total y balance para excel , agregados colores para balance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,12 @@ class ApplicationController < ActionController::Base
     book = Spreadsheet::Workbook.new
     sheet1 = book.create_worksheet :name => sheetname
     header_format = Spreadsheet::Format.new :color => :black, :weight => :bold
+    verde = Spreadsheet::Format.new :color => :green, :weight => :bold
+    rojo = Spreadsheet::Format.new :color => :red, :weight => :bold
     sheet1.row(0).default_format = header_format
-   
+    
+    
+    
     
 
     rownum = 0
@@ -19,10 +23,23 @@ class ApplicationController < ActionController::Base
         sheet1.row(rownum).push row[column].nil? ? 'N/A' : row[column]
       end
     end
-    rowactual = sheet1.row(rownum+2)
-      rowactual.push '','TOTAL',sumacargos,sumaabonos
-    rowactual = sheet1.row(rownum+3)
-      rowactual.push '','BALANCE',balancepos,balanceneg
+    
+  
+    row = sheet1.row(rownum+2)
+    row[1] = 'TOTAL'
+    row[2] = sumacargos
+    row[3] = sumaabonos
+    row.set_format(1,header_format ) # formato a la primera celda 
+    
+    row = sheet1.row(rownum+3)
+    row[1] = 'BALANCE'
+    row[2] = balancepos
+    row[3] = balanceneg
+    row.set_format(1,header_format ) # formato a la primera celda 
+    row.set_format(2,verde ) # formato verde para balancepos
+    row.set_format(3,rojo ) # formato rojo para balanceneg
+  
+      
     t = Time.now
     filename = "#{filename}_#{t.strftime("%Y%m%d%H%M%S")}"
     book.write "tmp/#{filename}.xls"
